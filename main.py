d8a7ae9e7a2d43c0596d4ea6a5b3f5738edda505
thoth2357 <98170427+thoth2357@users.noreply.github.com>
2022-04-11 17:21:14 +0100

    file done with

---
+++ main.py
@@ -0,0 +1,50 @@
+import os, shutil
+from pathlib import Path
+import time
+
+storage_directory = os.path.join(Path.home(), 'Documents')
+directory = "/home/pirate/Documents/Test_folder"
+# print(os.walk(directory)
+for i in os.walk(directory):
+    # print(i[0])
+    for file in i[2]:
+        directory_file = i[0]
+        extension = file.split('.')[1]
+        try:
+            # print(file)
+
+            extension_folder = os.path.join(storage_directory, f'arranged_folder/{extension}')
+            try:
+                os.makedirs(extension_folder)
+            except FileExistsError:
+                if Path(os.path.join(extension_folder, file)).is_file():
+                    duplicate_folder = os.path.join(extension_folder, 'Duplicates')
+                    try:
+                        os.makedirs(duplicate_folder)
+                        shutil.move(os.path.join(directory_file, file), duplicate_folder)
+                    except FileExistsError:
+                        try:
+                            shutil.move(os.path.join(directory_file, file), duplicate_folder)
+                        except Exception:
+                            os.rename(os.path.join(directory_file, file), os.path.join(directory_file, f"{file}__{time.time()}"))
+                            shutil.move(os.path.join(directory_file, f"{file}__{time.time()}"), duplicate_folder)
+
+                else:
+                    shutil.move(os.path.join(directory_file, file), extension_folder)
+        except FileExistsError:
+            if Path(os.path.join(extension_folder, file)).is_file():
+                duplicate_folder = os.path.join(extension_folder, 'Duplicates')
+                try:
+                    os.makedirs(duplicate_folder)
+                    shutil.move(os.path.join(directory_file, file), duplicate_folder)
+                except FileExistsError:
+                    try:
+                        shutil.move(os.path.join(directory_file, file), duplicate_folder)
+                    except Exception:
+                        os.rename(os.path.join(directory_file, file), os.path.join(directory_file, f"{file}__{time.time()}"))
+                        shutil.move(os.path.join(directory_file, f"{file}__{time.time()}"), duplicate_folder)
+            else:
+                shutil.move(os.path.join(directory_file, file), extension_folder)
+            
+
+            
